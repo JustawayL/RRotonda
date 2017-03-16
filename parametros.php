045eb8dee7b856437a29f3a6b1a7f4c080e1dfd0
Jeisson <jeisson005@hotmail.com>
2017-03-16 00:42:17 -0500

    Corrección de algunos errores

--- parametros.php
+++ parametros.php
@@ -1,6 +1,5 @@
 <?php
-	include 'modelo/Cliente.php';
 	$_SESSION["titulo"] = "Restaurante Tus Tacos";
 	$_SESSION["version"] = "1.0";
-	$_SESSION["usuario"] = new Cliente('Jeisson','Clave');
+	$_SESSION["usuario"] = null;
 ?>
